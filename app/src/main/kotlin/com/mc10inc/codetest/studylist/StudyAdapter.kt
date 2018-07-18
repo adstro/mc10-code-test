083e1a6cbbf1f6292e67a47cdc6a5e1f5184db11
Adam Stroud <adam.stroud@gmail.com>
2018-07-18 19:23:26 -0400

    Update libraries

--- app/src/main/kotlin/com/mc10inc/codetest/studylist/StudyAdapter.kt
+++ app/src/main/kotlin/com/mc10inc/codetest/studylist/StudyAdapter.kt
@@ -13,19 +13,19 @@ import com.mc10inc.codetest.model.Study
  * @author Adam Stroud &#60;[adam.stroud@gmail.com](mailto:adam.stroud@gmail.com)&#62;
  */
 class StudyAdapter : RecyclerView.Adapter<StudyViewHolder>() {
-    val studyList = emptyList<Study>().toMutableList()
+    private val studyList = emptyList<Study>().toMutableList()
 
     override fun getItemCount(): Int = studyList.size
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): StudyViewHolder =
-            StudyViewHolder(ListItemStudyBinding.inflate(LayoutInflater.from(parent?.context), parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudyViewHolder =
+            StudyViewHolder(ListItemStudyBinding.inflate(LayoutInflater.from(parent.context), parent, false))
 
-    override fun onBindViewHolder(holder: StudyViewHolder?, position: Int) {
+    override fun onBindViewHolder(holder: StudyViewHolder, position: Int) {
         val study = studyList[position]
 
-        holder?.binding?.title?.text = study.title
-        holder?.binding?.studyDisplayName?.text = study.displayName
-        holder?.binding?.dateTime?.text = DateUtils.formatDateTime(holder?.binding?.root?.context,
+        holder.binding.title.text = study.title
+        holder.binding.studyDisplayName.text = study.displayName
+        holder.binding.dateTime.text = DateUtils.formatDateTime(holder.binding.root.context,
                 study.createdTs.toEpochMilli(),
                 DateUtils.FORMAT_SHOW_TIME
                         or DateUtils.FORMAT_SHOW_DATE
